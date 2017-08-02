34404816efba3cca4b743334540b4988a2fecccb
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-08-02 15:11:54 +0430

    one-select fix for radio inputs

--- index.ts
+++ index.ts
@@ -3,14 +3,23 @@
 import * as $ from "jquery";
 $.fn.bootstrapCheckbox = function(){
 	function setClasses(){
-		const $label = $(this).parents("label");
-		const checked: boolean = $(this).prop("checked");
+		const $this = $(this);
+		const $label = $this.parents("label");
+		const checked: boolean = $this.prop("checked");
+		const isRadio: boolean = $this.attr("type").toLowerCase() === "radio";
 		if (checked){
 			if (!$label.hasClass("checked")){
 				$label.addClass("checked");
 			}
+			if (isRadio) {
+				const $form = $this.parents("form");
+				const name = $this.attr("name");
+				const $others = $(`input[name="${name}"][type=radio]`, $form).not($this);
+				$others.prop("checked", false);
+				$others.parents("label").removeClass("checked");
+			}
 			$label.removeClass("indeterminate");
-		}else{
+		}else if (!isRadio) {
 			$label.removeClass("checked");
 			const indeterminate: boolean = $(this).prop("checked");
 			if (indeterminate){
